d6c9eb9fd20fd39db7e4a5d67a0b0f09854ac115
AnikeshDey <anikeshdey2000@gmail.com>
2023-08-25 19:02:40 +0530

    first commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   solidity: {
     version: '0.8.9',
+    defaultNetwork: 'mumbai',
+    networks: {
+      hardhat: {},
+      mumbai: {
+        url: process.env.MUMBAI_RPC,
+        accounts: [process.env.PRIVATE_KEY],
+      }
+    },
     settings: {
       optimizer: {
         enabled: true,
